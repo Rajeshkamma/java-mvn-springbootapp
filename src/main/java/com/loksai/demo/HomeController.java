640b0947c0f2af9b1114c94b75ac91f96978def0
Rajeshkamma <rajeshkammait@gmail.com>
2026-03-15 11:23:39 +0530

    Create HomeController.java

---
+++ src/main/java/com/loksai/demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.loksai.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "LoksaiETA Application is running!";
+    }
+
+}
